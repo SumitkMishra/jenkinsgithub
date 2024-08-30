5870779dda97ff094b5577e5aa6e476739740fa1
sumit mishra <sumitkumarmishra10@gmail.com>
2024-08-30 13:03:54 +0530

    jenkins execution

---
+++ src/test/java/tests/NewTest1.java
@@ -0,0 +1,14 @@
+package tests;
+
+import org.junit.Test;
+
+public class NewTest1 {
+  @Test
+  public void test01() {
+	  System.out.println("test01");
+  }
+  @Test
+  public void test02() {
+	  System.out.println("test02");
+  }
+}
